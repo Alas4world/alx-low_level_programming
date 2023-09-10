fd5c3fe098eecb6be8d88d59ad94e507959ade67
Alas4world <asherifdeen89@gmail.com>
2023-09-09 17:01:11 -0700

    Converting from digit to char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: It prints all the single numbers of base 10 from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit <= 9 ; digit++)
+	{
+		putchar(digit + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
