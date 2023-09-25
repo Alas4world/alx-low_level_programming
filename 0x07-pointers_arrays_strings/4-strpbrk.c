07de521fb7a0019497bdd0ae39795eb729bf4fd4
Alas4world <asherifdeen89@gmail.com>
2023-09-25 07:54:33 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Function Name
+ * @s: Parameter 1
+ * @accept: Parameter 2
+ * Return: Always 0
+ */
+
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+	int j;
+
+	for (k = 0 ; s[k] != '\0' ; k++)
+	{
+		for (j = 0 ; accept[j] != '\0' ; j++)
+		{
+			if (s[k] == accept[j])
+				return (s + k);
+		}
+	}
+	return (0);
+}
